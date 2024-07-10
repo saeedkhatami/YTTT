a78f739cc60bc13a2838689fe9498d99c631bb1c
Saeed Khatami <0xb4dc0d3x@gmail.com>
2024-07-10 09:05:11 +0330

    added is_playlist function for optimizing output template (outtmpl) based on url type
    - introduced is_playlist function
    - updated get_ydl_options function
    - modified download_video function

--- YTTT.py
+++ YTTT.py
@@ -3,11 +3,19 @@ import yt_dlp
 def display_intro():
     print("YouTubeTerminalTerminal")
 
-def get_ydl_options(debug, quality):
+def is_playlist(url):
+    ydl_opts = {'quiet': True}
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        return 'entries' in info_dict
+
+def get_ydl_options(debug, quality, is_playlist):
+    outtmpl = './%(title)s.%(ext)s' if not is_playlist else './%(playlist_title)s/%(playlist_index)03d-%(title)s.%(ext)s'
+    
     common_opts = {
         'format': 'bestvideo+bestaudio/best',
         'merge_output_format': 'mp4',
-        'outtmpl': './%(playlist_title)s/%(playlist_index)03d-%(title)s.%(ext)s',
+        'outtmpl': outtmpl,
         'external_downloader': 'aria2c',
         'external_downloader_args': [
             '--min-split-size=1M',
@@ -35,7 +43,8 @@ def get_ydl_options(debug, quality):
     return common_opts
 
 def download_video(url, debug, quality):
-    ydl_opts = get_ydl_options(debug, quality)
+    playlist = is_playlist(url)
+    ydl_opts = get_ydl_options(debug, quality, playlist)
     
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
@@ -44,7 +53,7 @@ def menu():
     display_intro()
     debug_mode = input("DEBUG? (y/n): ").strip().lower() == 'y'
     url = input("Give me your YouTube URL: ").strip()
-    quality_mode = input("Quality(480/720/1080/best): ").strip().lower()
+    quality_mode = input("Quality (480/720/1080/best): ").strip().lower()
     download_video(url, debug_mode, quality_mode)
 
 if __name__ == "__main__":
